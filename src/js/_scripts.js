84ed42a652dd0e664280c6a8548d95532d6ee7b6
Вячеслав Ящук <vaceslavasuk@MacBook-Pro-Vaceslav.local>
2020-10-05 21:31:16 +0300

    add slider & photo

--- src/js/_scripts.js
+++ src/js/_scripts.js
@@ -1,11 +1,63 @@
 jQuery(function($){
 	'use strict';
 	
-// 	$(document).ready(function(){
-// 		$('.slider').slick({
-			
-// 		});
-// 	});
+	$(document).ready(function(){
+		$('.partners__items').slick({
+			dots: true,
+			slidesToShow: 4,
+			infinite: true,
+			speed: 500,
+			arrows: false,
+			autoplay: true
+		});
+	});
+
+	$('.volunteers__options').each(function(){
+		var $this = $(this), numberOfOptions = $(this).children('option').length;
+	  
+		$this.addClass('select-hidden'); 
+		$this.wrap('<div class="select"></div>');
+		$this.after('<div class="select-styled"></div>');
+	
+		var $styledSelect = $this.next('div.select-styled');
+		$styledSelect.text($this.children('option').eq(0).text());
+	  
+		var $list = $('<ul />', {
+			'class': 'select-options'
+		}).insertAfter($styledSelect);
+	  
+		for (var i = 0; i < numberOfOptions; i++) {
+			$('<li />', {
+				text: $this.children('option').eq(i).text(),
+				rel: $this.children('option').eq(i).val()
+			}).appendTo($list);
+		}
+	  
+		var $listItems = $list.children('li');
+	  
+		$styledSelect.click(function(e) {
+			e.stopPropagation();
+			$('div.select-styled.select-active').not(this).each(function(){
+				$(this).removeClass('select-active').next('ul.select-options').hide();
+			});
+			$(this).toggleClass('select-active').next('ul.select-options').toggle();
+		});
+	  
+		$listItems.click(function(e) {
+			e.stopPropagation();
+			$styledSelect.text($(this).text()).removeClass('select-active');
+			$this.val($(this).attr('rel'));
+			$list.hide();
+			//console.log($this.val());
+		});
+	  
+		$(document).click(function() {
+			$styledSelect.removeClass('select-active');
+			$list.hide();
+		});
+	
+	});
+
 // 	$(document).mouseup(function(e){ 
 // 		var main= $('.nav')
 // 		var div = $('.burger');
